caf96776eaf1df150f5ecc542817ce3ddfceb629
Lázaro Raúl <riglesiasvera@gmail.com>
2020-12-29 15:31:38 -0500

    008

---
+++ less than 1300/008 - Word Capitalization.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+#define all(x) x.begin(), x.end()
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> dlist;
+typedef vector<dlist> matrix;
+
+void solve(){
+    string s;
+    cin >> s;
+
+    if(s[0] >= 'a') s[0] -= 32;
+    cout << s << "\n";    
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t = 1;
+    while(t--) solve();
+
+    return 0;
+}//RUL0
