1290772fc10442c734603c0629943d125f6a97e2
Lázaro Raúl <riglesiasvera@gmail.com>
2020-12-29 15:56:06 -0500

    012

---
+++ less than 1300/012 - Ultra-Fast Mathematician.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+#define all(x) x.begin(), x.end()
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> dlist;
+typedef vector<dlist> matrix;
+
+void solve(){
+    string a, b;
+    cin >> a >> b;
+
+    for(int i = 0; i < a.size(); ++i)
+        cout << "01"[a[i] ^ b[i]];
+    cout << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t = 1;
+    while(t--) solve();
+
+    return 0;
+}//RUL0
