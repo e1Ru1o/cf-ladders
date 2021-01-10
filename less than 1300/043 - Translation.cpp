577db63fc1a2622bda93844ac8feac392fb8130d
Lázaro Raúl <riglesiasvera@gmail.com>
2021-01-09 22:37:57 -0500

    043

---
+++ less than 1300/043 - Translation.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+#define all(x) x.begin(), x.end()
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> dlist;
+typedef vector<dlist> matrix;
+
+void solve(){
+    string a, b;
+    cin >> a >> b;
+    reverse(all(a));    
+   
+    string ans[2] = {"NO\n", "YES\n"};
+    cout << ans[a == b];
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t = 1;
+    while(t--) solve();
+
+    return 0;
+}//RUL0
