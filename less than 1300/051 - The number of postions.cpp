11b80e9e8f9d24888f78b4c93e0516f36dc84d90
Lázaro Raúl <riglesiasvera@gmail.com>
2021-02-02 13:08:16 -0500

    051

---
+++ less than 1300/051 - The number of postions.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+#define all(x) x.begin(), x.end()
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> dlist;
+typedef vector<dlist> matrix;
+
+void solve(){
+    int n, a, b;
+    cin >> n >> a >> b;
+    cout << n - max(a, n - b - 1) << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t = 1;
+    while(t--) solve();
+
+    return 0;
+}//RUL0
